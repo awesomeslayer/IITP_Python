5d4399c5b39b991a4d8e8d10ed657863cc0295b5
awes0meslayer <89474980+awesomeslayer@users.noreply.github.com>
2024-03-28 00:57:14 +0300

    Added Poetry, CI/CD, pre-commit hook, linters, nox, etc.
    0) Renamed project to GIGAGOR for more comfortable usage
    1.5) Added Poetry for better dependencies resolving 
    1) Added pre-commit hooks for our project including other person's tests
    2) Added linters for beautiful view
    3) Added nox for session automatization
    4) Added CI/CD for github workflows usage 
    5) Refactoring

---
+++ Hough_main/src/gigagor/hough_transform.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def merge_histograms(h1, h2):
+    height = h1.shape[0]
+    width = 1 if len(h1.shape) == 1 else h1.shape[1]
+    n0 = width * 2
+    r = (width - 1) / (n0 - 1)
+    h1, h2 = h1.reshape((height, width)), h2.reshape((height, width))
+    result = np.zeros((height, n0))
+    for t in range(n0):
+        t0 = int(t * r)
+        s = t - t0
+        result[:, t] = h1[:, t0] + np.concatenate(
+            [h2[s:height, t0], h2[0:s, t0]], axis=0
+        )
+    return result
+
+
+def fast_hough_transform(Picture):
+    n = Picture.shape[1]
+    if n < 2:
+        return Picture[:, 0]
+    else:
+        return merge_histograms(
+            fast_hough_transform(Picture[:, 0 : int(n / 2)]),
+            fast_hough_transform(Picture[:, int(n / 2) : n]),
+        )
+
+
+def find_max_value(picture):
+    result = 0
+    a, b = picture.shape
+    s, t = 0.0, 0.0
+    for i in range(a):
+        for j in range(b):
+            if picture[i, j] > result:
+                result = picture[i, j]
+                s = i
+                t = j
+    return result, tuple((s, t))
+
+
+def find_point(n, s, t):
+    x = round((n - s) * n / t)
+    return x
---
+++ Hough_main/src/gigagor/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from .image_processing import read_image
+from .hough_transform import fast_hough_transform, find_max_value
+from .draw_line import draw_line, hough_transform
+
+if __name__ == "__main__":
+    FOLDER = "images/"
+    print("Input picture file name from /images folder:")
+
+    pic_name = input()
+    # fig, ax = plt.subplots()
+    # fig.set_figwidth(12)
+    # fig.set_figheight(12)
+    # fig.suptitle("Input Image")
+    img_new = read_image(FOLDER, pic_name)
+
+    # fig, ax = plt.subplots()
+    # fig.set_figwidth(12)
+    # fig.set_figheight(12)
+    # fig.suptitle("Lines Image")
+    max_hft, obr, line_img = hough_transform(img_new, draw=1)
+
+    fig, ax = plt.subplots()
+    ax.imshow(obr)
+    fig.set_figwidth(12)
+    fig.set_figheight(12)
+    fig.suptitle("Hough Image")
+    fig.savefig(f"test_output/hough_{pic_name}")
+
+    fig, ax = plt.subplots()
+    ax.imshow(line_img)
+    fig.set_figwidth(12)
+    fig.set_figheight(12)
+    fig.suptitle("Hough Lines Image")
+    fig.savefig(f"test_output/line_img_{pic_name}")
+
+    # plt.show()
