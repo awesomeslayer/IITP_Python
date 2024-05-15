bed6bef0449b693dd55764fe22c96b47c117fd30
fatalick <fatalick01@gmail.com>
2024-05-16 02:49:16 +0300

    edited nox, added typing+annotation, added readme

--- src/gigagor/hough_transform.py
+++ src/gigagor/hough_transform.py
@@ -1,24 +1,25 @@
 import numpy as np
+from typing import Tuple
 
 
-def merge_histograms(h1, h2):
-    height = h1.shape[0]
-    width = 1 if len(h1.shape) == 1 else h1.shape[1]
-    n0 = width * 2
-    r = (width - 1) / (n0 - 1)
+def merge_histograms(h1: np.ndarray, h2: np.ndarray) -> np.ndarray:
+    height: int = h1.shape[0]
+    width: int = 1 if len(h1.shape) == 1 else h1.shape[1]
+    n0: int = width * 2
+    r: float = (width - 1) / (n0 - 1)
     h1, h2 = h1.reshape((height, width)), h2.reshape((height, width))
-    result = np.zeros((height, n0))
+    result: np.ndarray = np.zeros((height, n0))
     for t in range(n0):
-        t0 = int(t * r)
-        s = t - t0
+        t0: int = int(t * r)
+        s: int = t - t0
         result[:, t] = h1[:, t0] + np.concatenate(
             [h2[s:height, t0], h2[0:s, t0]], axis=0
         )
     return result
 
 
-def fast_hough_transform(Picture):
-    n = Picture.shape[1]
+def fast_hough_transform(Picture: np.ndarray) -> np.ndarray:
+    n: int = Picture.shape[1]
     if n < 2:
         return Picture[:, 0]
     else:
@@ -28,8 +29,8 @@ def fast_hough_transform(Picture):
         )
 
 
-def find_max_value(picture):
-    result = 0
+def find_max_value(picture: np.ndarray) -> Tuple[float, Tuple[float, float]]:
+    result: float = 0
     a, b = picture.shape
     s, t = 0.0, 0.0
     for i in range(a):
@@ -41,6 +42,6 @@ def find_max_value(picture):
     return result, tuple((s, t))
 
 
-def find_point(n, s, t):
-    x = round((n - s) * n / t)
+def find_point(n: int, s: float, t: float) -> float:
+    x: float = round((n - s) * n / t)
     return x
--- src/gigagor/main.py
+++ src/gigagor/main.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 from .image_processing import read_image
-from .hough_transform import fast_hough_transform, find_max_value
-from .draw_line import draw_line, hough_transform
+from .draw_line import hough_transform
+from typing import Any
 
 if __name__ == "__main__":
-    FOLDER = "images/"
+    FOLDER: str = "images/"
     print("Input picture file name from /images folder:")
 
-    pic_name = input()
+    pic_name: str = input()
     # fig, ax = plt.subplots()
     # fig.set_figwidth(12)
     # fig.set_figheight(12)
     # fig.suptitle("Input Image")
-    img_new = read_image(FOLDER, pic_name)
+    img_new: Any = read_image(FOLDER, pic_name)
 
     # fig, ax = plt.subplots()
     # fig.set_figwidth(12)
     # fig.set_figheight(12)
     # fig.suptitle("Lines Image")
+    max_hft: float
+    obr: Any
+    line_img: Any
     max_hft, obr, line_img = hough_transform(img_new, draw=1)
 
     fig, ax = plt.subplots()
